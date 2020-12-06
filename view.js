5e0c28dd42028acf14db4063b3d262bfe4c879c5
Chris Thomas <tchrs@uber.com>
2020-12-06 10:27:25 -0500

    order by duration instead of timestamp

--- view.js
+++ view.js
@@ -1,6 +1,6 @@
 /* eslint-env browser */
 
-const {DateTime} = luxon;
+const {DateTime, Duration} = luxon;
 
 const statsJsonUri = 'https://raw.githubusercontent.com/Xiot/xiot.github.io/master/2020.json';
 const trophySvg = createTrophy();
@@ -26,6 +26,8 @@ function initialize(data) {
     const grid = document.getElementById('ranking-grid');
     stats = dataByDay(data);
 
+    console.log(stats);
+
     append(grid, [
         div({class: 'day title'}, 'day'),
         div({class: 'name title'}, 'name'),
@@ -40,9 +42,9 @@ function initialize(data) {
         append(grid, [
             div({class: 'day link value', onclick: () => showStatsForDay(s.day)}, s.day.toString()),
             div({class: 'name value'}, winner.name),
-            div({class: 'time value'}, formatTimestamp(s.day, winner.star1)),
+            div({class: 'time value'}, formatStarTime(winner.star1)),
             trophy(getPosition(s, 1, winner.star1)),
-            div({class: 'time value'}, formatTimestamp(s.day, winner.star2)),
+            div({class: 'time value'}, formatStarTime(winner.star2)),
             trophy(getPosition(s, 2, winner.star2)),
         ]);
     });
@@ -53,11 +55,11 @@ function getPosition(day, star, ts) {
 
     const sorted = [...day.scores]
         .map(member => {
-            return member[`star${star}`];
+            return member[`star${star}`].duration;
         })
     .sort((l, r) => l - r);
 
-    return sorted.indexOf(ts);
+    return sorted.indexOf(ts.duration);
 }
 
 function showStatsForDay(dayIndex) {
@@ -72,26 +74,26 @@ function showStatsForDay(dayIndex) {
     const sorted = [...day.scores].sort((l, r) => {
 
         const {
-            star1: l1 = Number.MAX_SAFE_INTEGER,
-            star2: l2 = Number.MAX_SAFE_INTEGER
+            star1: l1 = {duration: Number.MAX_SAFE_INTEGER},
+            star2: l2 = {duration: Number.MAX_SAFE_INTEGER}
         } = l;
         const {
-            star1: r1 = Number.MAX_SAFE_INTEGER,
-            star2: r2 = Number.MAX_SAFE_INTEGER
+            star1: r1 = {duration: Number.MAX_SAFE_INTEGER},
+            star2: r2 = {duration: Number.MAX_SAFE_INTEGER}
         } = r;
 
-        return r2 === l2
-            ? l1 - r1
-            : l2 - r2;
+        return r2.duration === l2.duration
+            ? l1.duration - r1.duration
+            : l2.duration - r2.duration;
     });
 
     sorted.forEach((user, index) => {
         append(el, [
             div({class: 'day value'}, (index + 1).toString()),
             div({class: 'name value'}, user.name),
-            div({class: 'time value'}, formatTimestamp(dayIndex, user.star1)),
+            div({class: 'time value'}, formatStarTime(user.star1)),
             trophy(getPosition(day, 1, user.star1)),
-            div({class: 'time value'}, formatTimestamp(dayIndex, user.star2)),
+            div({class: 'time value'}, formatStarTime(user.star2)),
             trophy(getPosition(day, 2, user.star2)),
         ]);
     });
@@ -103,7 +105,7 @@ function fastestScore(scores, star) {
     }
     const key = `star${star}`;
     return scores.reduce((fastest, score) => {
-        if (fastest[key] > score[key]) {
+        if (fastest[key].duration > score[key].duration) {
             return score;
         }
         return fastest;
@@ -131,7 +133,33 @@ function dataByDay(data) {
 
 function getStarTime(member, day, star) {
     const text = get(member, ['completion_day_level', day, star, 'get_star_ts']);
-    return text ? parseInt(text, 10) * 1000 : undefined;
+    const timestamp = text ? parseInt(text, 10) * 1000 : undefined;
+    if (!timestamp) return undefined;
+    return {
+        timestamp,
+        duration: duration(day, timestamp)
+    }
+}
+
+function duration(day, ts) {
+    if (!ts) return undefined;
+
+    const startOfDay = DateTime.local(2020, 12, 1)
+        .setZone('America/Toronto', {keepLocalTime: true})
+        .plus({days: day - 1});
+
+    let duration = DateTime.fromMillis(ts).diff(startOfDay);
+
+    const newStartTime = duration.as('milliseconds') > startOffset;
+    if (newStartTime)
+        duration = duration.plus({milliseconds: -startOffset});
+
+    return duration.as('milliseconds');
+}
+
+function formatStarTime(star) {
+    if (!star || !star.duration) return '';
+    return Duration.fromMillis(star.duration).toFormat('hh:mm:ss');
 }
 
 function formatTimestamp(day, ts) {
@@ -144,7 +172,8 @@ function formatTimestamp(day, ts) {
 
     let duration = DateTime.fromMillis(ts).diff(startOfDay);
 
-    if (duration.as('milliseconds') > startOffset)
+    const newStartTime = duration.as('milliseconds') > startOffset;
+    if (newStartTime)
         duration = duration.plus({milliseconds: -startOffset});
 
     return duration.toFormat('hh:mm:ss');
@@ -191,13 +220,9 @@ function createTrophy() {
     const container = document.createElement('div');
     const el = document.createElement('svg');
     container.appendChild(el);
-    // container.innerHTML = `
-    //     <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="trophy" class="svg-inline--fa fa-trophy fa-w-18" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
-    //         <path fill="currentColor" d="M552 64H448V24c0-13.3-10.7-24-24-24H152c-13.3 0-24 10.7-24 24v40H24C10.7 64 0 74.7 0 88v56c0 35.7 22.5 72.4 61.9 100.7 31.5 22.7 69.8 37.1 110 41.7C203.3 338.5 240 360 240 360v72h-48c-35.3 0-64 20.7-64 56v12c0 6.6 5.4 12 12 12h296c6.6 0 12-5.4 12-12v-12c0-35.3-28.7-56-64-56h-48v-72s36.7-21.5 68.1-73.6c40.3-4.6 78.6-19 110-41.7 39.3-28.3 61.9-65 61.9-100.7V88c0-13.3-10.7-24-24-24zM99.3 192.8C74.9 175.2 64 155.6 64 144v-16h64.2c1 32.6 5.8 61.2 12.8 86.2-15.1-5.2-29.2-12.4-41.7-21.4zM512 144c0 16.1-17.7 36.1-35.3 48.8-12.5 9-26.7 16.2-41.8 21.4 7-25 11.8-53.6 12.8-86.2H512v16z"></path>
-    //     </svg>
-    // `;
+
     container.innerHTML = `
-    <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="star" class="svg-inline--fa fa-star fa-w-18" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path fill="currentColor" d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"></path></svg>
+        <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="star" class="svg-inline--fa fa-star fa-w-18" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path fill="currentColor" d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"></path></svg>
     `
     return container.firstElementChild;
 }
